0d8681fdba9e8c2be27e647238e92843e024c174
JoongWon SHIN <joongwon0204@gmail.com>
2024-10-23 22:27:44 +0900

    #5525 IOIOI

---
+++ C++/tempCodeRunnerFile.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+bool canCountWith(char prev, char curr, int counter) {
+    if (prev != curr){
+        if (counter != 0) { return true;}
+        if (prev == 'I') { return true;}
+    }
+    return false;
+}
+
+int main() {
+    int n, m, i, ans = 0, counter = 0;
+    cin >> n >> m;
+    char s[m+1];
+    cin >> s;
+
+    char currChar, prevChar;
+    for (i=1; i<m; i++) {
+        currChar = s[i];
+        if (canCountWith(prevChar,currChar,counter)) {
+            counter++;
+        } else {
+            if (counter != 0) {
+                int currP = counter/2 - n + 1;
+                if (currP > 0) { ans += currP;}
+                counter = 0;
+            }
+        }
+        prevChar = currChar;
+    }
+    int lastP = counter/2 - n + 1;
+    if (lastP > 0){ ans += lastP;}
+    cout << ans;
+}
