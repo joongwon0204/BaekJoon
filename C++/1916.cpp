deee47a60f636743c8554100f56b74bd9543d84c
JoongWon SHIN <joongwon0204@gmail.com>
2024-12-14 11:44:43 +0900

    1916

--- C++/1916.cpp
+++ C++/1916.cpp
@@ -1,7 +1,8 @@
-// https://www.acmicpc.net/problem/
+// https://www.acmicpc.net/problem/1916
 #include <iostream>
 #include <string>
 #include <vector>
+#include <numeric>
 
 using namespace std;
 
@@ -13,5 +14,19 @@ void init() {
 int main() {
     init();
 
-    
+    int n, m;
+    cin >> n >> m;
+
+    vector<int> cities(n);
+    iota(cities.begin(), cities.end(), 1);
+
+    vector<vector<pair<int, int>>> buses(n+1);
+    int u, v, t;
+    for (int i = 0; i < m; i++) {
+        cin >> u >> v >> t;
+        buses[u].push_back(make_pair(v,t));
+    }
+
+    int s, e;
+    cin >> s >> e;
 }
