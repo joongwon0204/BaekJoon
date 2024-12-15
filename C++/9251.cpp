49c5a51282f0e789207ed81016145b5b33fe21c0
JoongWon SHIN <joongwon0204@gmail.com>
2024-12-15 21:29:08 +0900

    9251

---
+++ C++/9251.cpp
@@ -0,0 +1,33 @@
+// https://www.acmicpc.net/problem/9251
+#include <iostream>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+void init() {
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+}
+
+int main() {
+    init();
+
+    string a, b;
+    cin >> a >> b;
+
+    int la = a.size(), lb = b.size();
+    vector<vector<int>> cache(la + 1, vector<int>(lb + 1, -1));
+
+    for (int i = 1; i <= la; i++) {
+        for (int j = 1; j <= lb; j++) {
+            if (a[i - 1] == b[j - 1]) {
+                cache[i][j] = cache[i - 1][j - 1] + 1;
+            } else {
+                cache[i][j] = max(cache[i - 1][j], cache[i][j - 1]);
+            }
+        }
+    }
+
+    cout << cache[la][lb];
+}
