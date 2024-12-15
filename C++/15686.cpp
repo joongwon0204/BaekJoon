f179d980bb686ca04db277cdf0eb8031c14dbd07
JoongWon SHIN <joongwon0204@gmail.com>
2024-12-15 19:46:29 +0900

    15686

---
+++ C++/15686.cpp
@@ -0,0 +1,80 @@
+// https://www.acmicpc.net/problem/15686
+#include <iostream>
+#include <string>
+#include <vector>
+#include <cmath>
+
+using namespace std;
+
+void init() {
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+}
+
+int min_d = 1234567;
+
+void solve(int prev_c, int left, vector<bool>& sellected, const int& c, const int& h, const vector<vector<int>>& ds) {
+    if (left == 0) {
+        int temp, cur_d = 0;
+        for (int i = 0; i < h; i++) {
+            temp = 1234567;
+            for (int j = 0; j < c; j++) {
+                if (!sellected[j]) { continue; }
+                temp = min(temp, ds[j][i]);
+            }
+            cur_d += temp;
+        }
+        min_d = min(min_d, cur_d);
+        return;
+    }
+
+    for (int i = prev_c + 1; i < c - left + 1; i++) {
+        sellected[i] = true;
+        solve(i, left - 1, sellected, c, h, ds);
+        sellected[i] = false;
+    }
+}
+
+
+int main() {
+    init();
+
+    int n, m, temp;
+    cin >> n >> m;
+    vector<pair<int, int>> houses;
+    vector<pair<int, int>> chickens;
+    pair<int, int> house, chicken;
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n; j++) {
+            cin >> temp;
+            if (temp == 1) {
+                houses.push_back(make_pair(i,j));
+            } else if (temp == 2) {
+                chickens.push_back(make_pair(i,j));
+            }
+        }
+    }
+
+    int h = houses.size(), c = chickens.size();
+
+    vector<vector<int>> ds(c, vector<int>(h, 0));
+    for (int i = 0; i < c; i++) {
+        chicken = chickens[i];
+        for (int j = 0; j < h; j++) {
+            house = houses[j];
+            ds[i][j] = abs(chicken.first - house.first) + abs(chicken.second - house.second);
+        }
+    }
+
+    for (int i = 0; i < c; i++) {
+        cout << '\n';
+        for (int j = 0; j < h; j++) {
+            cout << ds[i][j] << ' ';
+        }
+    }
+    vector<bool> sellected(c, false);
+    solve(-1, m, sellected, c, h, ds);
+
+    cout << min_d;
+
+}
