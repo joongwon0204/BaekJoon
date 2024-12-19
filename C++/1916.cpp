dd155009b0c230fb23ee48b57160ca0755f71be0
JoongWon SHIN <joongwon0204@gmail.com>
2024-12-20 05:21:13 +0900

    1916

--- C++/1916.cpp
+++ C++/1916.cpp
@@ -2,7 +2,7 @@
 #include <iostream>
 #include <string>
 #include <vector>
-#include <numeric>
+#include <algorithm>
 
 using namespace std;
 
@@ -17,16 +17,34 @@ int main() {
     int n, m;
     cin >> n >> m;
 
-    vector<int> cities(n);
-    iota(cities.begin(), cities.end(), 1);
-
-    vector<vector<pair<int, int>>> buses(n+1);
+    vector<vector<pair<int, int>>> buses(n + 1);
+    vector<int> distance(n + 1, 100000001);
     int u, v, t;
     for (int i = 0; i < m; i++) {
         cin >> u >> v >> t;
-        buses[u].push_back(make_pair(v,t));
+        buses[u].push_back(make_pair(v, t));
     }
 
     int s, e;
     cin >> s >> e;
+
+    priority_queue<pair<int, int>, vector<pair<int, int>>, greater<pair<int, int>>> pq;
+    pair<int, int> cur;
+    pq.push({0, s});
+    distance[s] = 0;
+    while (!pq.empty()) {
+        cur = pq.top();
+        pq.pop();
+
+        if (cur.first > distance[cur.second]) continue;
+
+        for (auto& bus: buses[cur.second]) {
+            if (distance[bus.first] > distance[cur.second] + bus.second) {
+                distance[bus.first] = distance[cur.second] + bus.second;
+                pq.push({distance[bus.first], bus.first});
+            }
+        }
+    }
+
+    cout << distance[e];
 }
