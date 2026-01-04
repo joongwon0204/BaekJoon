635f080928fbd1c21125977b19387bd21af0d448
JoongWon SHIN <joongwon0204@gmail.com>
2026-01-04 16:44:04 +0900

    26.01.04

---
+++ C++/12015.cpp
@@ -0,0 +1,32 @@
+// https://www.acmicpc.net/problem/12015
+#include <iostream>
+#include <string>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+void init() {
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+}
+
+int main() {
+    init();
+    
+    int N;
+    cin >> N;
+
+    vector<int> seq(N);
+    for (int i = 0; i < N; i++) {
+        cin >> seq[i];
+    }
+
+    vector<int> tails;
+    for (int x : seq) {
+        auto it = lower_bound(tails.begin(), tails.end(), x);
+        if (it == tails.end()) tails.push_back(x);
+        else *it = x;
+    }
+    cout << tails.size();
+}
